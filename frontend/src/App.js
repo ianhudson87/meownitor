c1d4c567997ca76d596350230f9d7e6f846877db
ianhudson87 <ihudson@wustl.edu>
2022-02-21 18:26:40 -0600

    api call to get data from backend. display on front end when received.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,55 +2,34 @@
 import React, {Component} from 'react'
 import LineChartTemplate from './Components/LineChartTemplate';
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
-
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      "data": null
+    }
+  }
+
+  componentDidMount(){
+    fetch("/api") // looks at the "proxy" field in the package.json to know the address/port
+    .then(res => res.json())
+    .then(
+        (results) => {
+          this.setState({
+            "data": results.data
+          })
+          console.log(results)
+        },
+        (error) => {
+          console.log(error)
+        }
+
+    )
+  }
+
 	render() {
 		return (
-      <LineChartTemplate data={data}/>
+      <LineChartTemplate data={this.state.data}/>
 		);
 	}
 }
